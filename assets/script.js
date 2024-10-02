68f733fa3ff338162e02d507e81cec77ab82c6b1
HMT Dev <hmtdev0@gmail.com>
2024-10-02 21:57:54 +0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.nav-toggler').addEventListener('click', function () {
+    this.classList.toggle('active');
+    document.querySelector('.navbar').classList.toggle('active');
+});
